7c98a7e0ebb7a917bc23f15020ad0bdf57a3ae38
Tom Elliott <tomelliott@fb.com>
2012-01-05 16:02:01 +0000

    Add very simple response to index to load html page

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+//    response.send('Hello World!');
+      var resp = fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
+        response.send(text);
+      });
 });
 
 var port = process.env.PORT || 3000;
